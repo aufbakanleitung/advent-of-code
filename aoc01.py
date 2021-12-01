059e5eb24ccc3e21c1634e3ee000e941f4ed0086
Herman van der Veer <hvdveer@gmail.com>
2021-12-01 14:45:57 +0100

    add assignment 01b

--- aoc01.py
+++ aoc01.py
@@ -1,9 +1,11 @@
 test = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
 report = [int(line.rstrip('\n')) for line in open("input_aoc01.txt")]
-print(report)
-larger_count = 0
+count = 0
 
-for i in range(len(report)-1):
-    if report[i+1] > report[i]:
-        larger_count += 1
-print(larger_count)
+for i in range(len(report)-3):
+    window_a = report[i] + report[i+1] + report[i+2]
+    window_b = report[i+1] + report[i+2] + report[i+3]
+    if window_b > window_a:
+        count += 1
+
+print(count)
