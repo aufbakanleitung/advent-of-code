343793bb02700455e257445550aa5238c91d7cab
Herman van der Veer <hvdveer@gmail.com>
2021-12-06 13:56:44 +0100

    06 solution

---
+++ aoc06.py
@@ -0,0 +1,24 @@
+# --- Day 6: Lanternfish ---
+school = [int(days) for days in open('input/06_input.txt').read().split(',')]
+
+def count_fish(school):
+    fish_age = {}
+    for c in range(9):
+        fish_age[c] = school.count(c)
+    return fish_age
+fish_age = count_fish(school)
+
+
+def next_day():
+    zero_fish = fish_age[0]
+    for c in range(0, 8):
+        fish_age[c] = fish_age[c+1]
+    fish_age[8] = zero_fish
+    fish_age[6] += zero_fish
+
+
+def simulate(days):
+    for day in range(days+1):
+        print(f"Day {day}\t Total fish: {sum(fish_age.values())}")
+        next_day()
+simulate(256)
