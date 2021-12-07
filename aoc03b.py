ae4235a3b6a923577d2a6ab978a7113f998d812e
Herman van der Veer <hvdveer@gmail.com>
2021-12-07 10:02:23 +0100

    Insufficient IQ

--- aoc03b.py
+++ aoc03b.py
@@ -1,32 +1,51 @@
 # --- Day 3b: Binary Diagnostic ---
+from pprint import pp
+
 lines = [line for line in open('input/03_example.txt').read().splitlines()]
+pp(lines)
 
 # What is the life support rating of the submarine?
 oxygen = CO2 = 0
-def counter(lines):
-    count = [0] * len(lines[0])
+def count_most(lines, bit):
+    count = 0
     for line in lines:
-        for i, c in enumerate(line):
-            if c == '1':
-                count[i] += 1
-    most = [0] * len(lines[0])
-    least = [1] * len(lines[0])
-    for i in range(len(lines[0])):
-        if count[i] > len(lines) // 2:
-            most[i] = 1
-        else:
-            least[i] = 0
-    return most, least
+        if line[bit] == '1':
+            count += 1
+    if count > len(lines) // 2:
+        return 1
+    else:
+        return 0
 
-count = (counter(lines))
+count = (count_most(lines, 0))
 print(count)
-gamma = epsilon = 0
 
-for line in lines:
-    for i in range(len(lines)):
-        most, least = counter(lines)
-        if line[i] != most[i]:
-            print('remove')
-            print(lines)
-            lines.remove(line)
-print(lines)
+gamma = lines[:]
+epsilon = lines[:]
+for i in range(len(lines[0])):
+    gamma_remove = []
+    eps_remove = []
+    if len(gamma) == 1: print(f"gamma: {gamma}")
+    if len(eps_remove) == 1: print(f"epsilon: {epsilon}")
+    if count_most(lines, i):
+        for line in lines:
+            if line[i]:
+                gamma_remove.append(line)
+            if not line[i]:
+                eps_remove.append(line)
+    for rm in gamma_remove:
+        if rm in gamma:
+            gamma.remove(rm)
+    for rm in eps_remove:
+        epsilon.remove(rm)
+
+# for i in range(len(lines[0])):
+#     if count_most(lines, i):
+#         for lines in lines:
+#             gamma.
+#
+#     most, least = counter(lines)
+#     if line[i] != most[i]:
+#         print('remove')
+#         print(lines)
+#         lines.remove(line)
+# print(lines)
