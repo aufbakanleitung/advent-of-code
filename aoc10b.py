d1a5e24cc4d4d1d3f4f0ef913425b756836feb71
Herman van der Veer <hvdveer@gmail.com>
2021-12-13 11:24:55 +0100

    Solve 8a,9a,10

--- aoc10b.py
+++ aoc10b.py
@@ -1,10 +1,7 @@
 # --- Day 10: Syntax Scoring ---
-from pprint import pp
-
 lines = open('input/10_input.txt').read().splitlines()
 
 haakje_dict = {'(':')','{':'}','[':']','<':'>'}
-points = {')':3,']':57,'}':1197,'>':25137}
 def not_corrupt(line):
     state = []
     for s in line:
@@ -13,10 +10,8 @@ def not_corrupt(line):
         elif haakje_dict[state[-1]] == s:
             state.pop()
         else:
-            # print(f"Corrupt! Found illegal {s}")
             return False
     return state
-
 
 
 scores = {'(':1,'[':2,'{':3,'<':4}
@@ -24,16 +19,15 @@ score_list = []
 for line in lines:
     state = not_corrupt(line)
     if state:
-        print(state)
         score = 0
         for s in reversed(state):
             score *= 5
             score += scores[s]
         score_list.append(score)
-score_list.sort()
-print(score_list[len(score_list)//2])
-
 
 # Find the completion string for each incomplete line, score the completion strings,
 # and sort the scores. What is the middle score?
 # print(f"The corrupt lines sum to: {sum}")
+score_list.sort()
+print(score_list[len(score_list)//2])
+
