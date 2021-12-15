f6ddf6f279b9fadf2d95e13cdceb59964a89d9b1
Herman van der Veer <hvdveer@gmail.com>
2021-12-15 23:56:33 +0100

    Solved 14b

--- aoc14b.py
+++ aoc14b.py
@@ -1,7 +1,7 @@
 # --- Day 14: Extended Polymerization ---
 from helpers import timer
 
-polymer, ins = open('input/14_example.txt').read().split('\n\n')
+polymer, ins = open('input/14_input.txt').read().split('\n\n')
 ins = {insert.split(' -> ')[0]:insert.split(' -> ')[1] for insert in ins.splitlines()}
 pc = dict.fromkeys(ins, 0)
 
@@ -21,18 +21,23 @@ def insert(pc):
 
 
 def count_letters(pc):
-    letters = dict.fromkeys(set('NCNBCHB'), 0)
+    letters = dict.fromkeys(set('PHOSBSKBBBFSPPPCCCHNV'), 0)
+    letters[polymer[-1]] += 1  # add the last letter of the polymer, because else it isn't counted
     for i in pc:
         letters[i[0]] += pc[i]
-        letters[i[1]] += pc[i]
-    print(letters)
+        # letters[i[1]] += pc[i]
     return letters
+print(count_letters({'CH': 1, 'HB': 1, 'NC': 1, 'NB': 1, 'BC': 1, 'CN': 1}))
+
+def plen(pc):
+    return sum(pc.values()) + 1
 
 pc = count_pairs(polymer)
 # Do steps
-for i in range(9):
+for i in range(40):
     pc = insert(pc)
-    print(i+1, pc)
+    print(f"#{i+1} len: {plen(pc)}  count: {count_letters(pc)}\n {pc}")
+
 
 letters = count_letters(pc)
 print("max-min difference: ", max(letters.values()) - min(letters.values()))
