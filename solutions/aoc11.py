5ae243f556db886605845d3d2fcb8b4978fe867c
Herman van der Veer <hvdveer@gmail.com>
2021-12-19 16:00:12 +0100

    Rescurive solve 11

--- solutions/aoc11.py
+++ solutions/aoc11.py
@@ -1,42 +1,49 @@
 # --- Day 11: Dumbo Octopus ---
 from pprint import pp
-lines = open('../input/11_example.txt').read().splitlines()
+lines = open('../input/11_input.txt').read().splitlines()
 lines = [list(map(int, line)) for line in lines]
 y_size, x_size = len(lines), len(lines[0])
 pp(lines)
 
-flash = 0
 
+# This doesn't work, flashes don't propagate upwards.
+# I'll leave it here as a warning for future generations
 def add_adjacent(y,x):
     adj = [(-1,-1),(-1,0),(-1,1),(0,-1),(0,1),(1,-1),(1,0),(1,1)]
     for yi,xi in adj:
         if 0 <= x+xi < x_size and 0 <= y+yi < y_size:
             lines[y+yi][x+xi] += 1
 
+flash = 0
+def recursive_add(lines, y, x):
+    global flash
+    flash += 1
+    for ly in (y-1, y, y+1):
+        for lx in (x-1, x, x+1):
+            if ly == y and lx == x: continue
+            if 0 <= ly < y_size and 0 <= lx < x_size:
+                lines[ly][lx] += 1
+                if lines[ly][lx] == 10:
+                    recursive_add(lines, ly, lx)
+                    lines[ly][lx] += 1
+
 
 def next(lines):
     global flash
-    reset = set()
-    for y in range(len(lines)):
-        for x in range(len(lines[0])):
+    for y in range(y_size):
+        for x in range(x_size):
             lines[y][x] += 1
+            if lines[y][x] == 10:
+                recursive_add(lines, y, x)
+                lines[y][x] += 1
 
-    for y in range(len(lines)):
-        for x in range(len(lines[0])):
+    for y in range(y_size):
+        for x in range(x_size):
             if lines[y][x] > 9:
-                add_adjacent(y,x)
-    print('before reset')
-    # pp(lines)
-    for y in range(len(lines)):
-        for x in range(len(lines[0])):
-            if lines[y][x] > 9:
-                flash += 1
-                # reset.add((y,x))
                 lines[y][x] = 0
 
-    # for y,x in reset:
-    #     lines[y][x] = 0
     return lines
+
 
 def string_print(grid):
     for line in grid:
@@ -44,13 +51,13 @@ def string_print(grid):
     print()
 
 
-for i in range(1,4):
-    print('step:', i)
-    string_print(next(lines))
-    print(f"{flash} flashes\n")
-
-
-
-
-# How many total flashes are there after 100 steps?
-
+def run():
+    for i in range(1,500):
+        print('step', i)
+        previous_flash = flash
+        string_print(next(lines))
+        print(f"{flash} flashes\n")
+        if flash - previous_flash == y_size * x_size:
+            print(f"Simultaneous flash!")
+            break
+run()
