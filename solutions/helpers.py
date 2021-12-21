66aee17e47deebea29ddbd96d19f545f6a7b7e2d
Herman van der Veer <hvdveer@gmail.com>
2021-12-22 00:57:34 +0100

    Solve 15: Depth first search (cheated a bit)

--- solutions/helpers.py
+++ solutions/helpers.py
@@ -10,3 +10,28 @@ def timer(func):
         print(f"\nTime required: {(time() - start_time)*1000:.2f} ms")
         return result
     return wrapper
+
+
+def string_print(grid, spacer=''):
+    for line in grid:
+        print(spacer.join(str(x) for x in line))
+    print()
+
+
+class expanding_range_2d:
+
+    def __init__(self, start_value1, start_value2):
+        self.start_value1 = start_value1
+        self.start_value2 = start_value2
+
+    def __iter__(self):
+        yield self.start_value1, self.start_value2
+
+        radius = 1
+        while True:
+            for i in range(-radius, radius):
+                yield self.start_value1+i, self.start_value2-radius
+                yield self.start_value1+radius, self.start_value2+i
+                yield self.start_value1+radius-i-1, self.start_value2+radius
+                yield self.start_value1-radius, self.start_value2+radius-i-1
+            radius += 1
