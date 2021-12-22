8e86549695311d012e227a701f6b4eae6fcaebb5
Herman van der Veer <hvdveer@gmail.com>
2021-12-22 14:47:17 +0100

    Refactor 15

--- solutions/aoc15.py
+++ solutions/aoc15.py
@@ -9,32 +9,33 @@ grid = [list(map(int, line)) for line in grid]
 @timer
 def shortest_path(grid):
     y_size, x_size = len(grid),len(grid[0])
-    paths = [(0,0,0)]
+    paths = [(0,0,0)]  # total, y, x
     visited = [[0] * len(row) for row in grid]
     while True:
-        total, x, y = heapq.heappop(paths)
-        if visited[x][y]: continue
-        if (x, y) == (x_size - 1, y_size - 1):
+        total, y, x = heapq.heappop(paths)  # Get coordinates for lowest path
+        if visited[y][x]: continue
+        if (y, x) == (y_size - 1, x_size - 1):
             return total
-        visited[x][y] = 1
-        for nx, ny in [(x, y + 1), (x + 1, y), (x - 1, y), (x, y - 1)]:
-            if not y_size > nx >= 0 <= ny < x_size: continue
-            if visited[nx][ny]: continue
-            heapq.heappush(paths, (total + grid[nx][ny], nx, ny))
+        visited[y][x] = 1
+        for ny, nx in [(y+1, x), (y, x+1), (y-1, x), (y, x-1)]:  # prefer down and right
+            if not x_size > ny >= 0 <= nx < y_size: continue
+            if visited[ny][nx]: continue
+            heapq.heappush(paths, (total + grid[ny][nx], ny, nx))
 
 print(f"Shortest path: {shortest_path(grid)}")
 
 # --- Part Two ---
-grid2 = [[0] * len(row) * 5 for row in grid * 5]
-y_size, x_size = len(grid),len(grid[0])
-
 def wrap(x):
     return (x - 1) % 9 + 1
 
-for y in range(len(grid2)):
-    for x in range(len(grid2[0])):
-        grid2[y][x] = wrap(grid[y % y_size][x % x_size] + y // y_size + x // x_size)
+def unfold_map(grid):
+    grid2 = [[0] * len(row) * 5 for row in grid * 5]
+    y_size,x_size = len(grid),len(grid[0])
+    for y in range(len(grid2)):
+        for x in range(len(grid2[0])):
+            grid2[y][x] = wrap(grid[y % y_size][x % x_size] + y // y_size + x // x_size)
+    return grid2
 
 # string_print(grid2, ' ')
-
+grid2 = unfold_map(grid)
 print(f"Full map shortest path: {shortest_path(grid2)}")
