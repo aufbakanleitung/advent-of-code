7d328cf208a05f16231b8651b9d5b90afaa12b81
Herman van der Veer <herman.van.der.veer-cic@ibm.com>
2023-12-12 17:00:52 +0100

    improved version

--- 2023/solutions/04.py
+++ 2023/solutions/04.py
@@ -2,7 +2,7 @@
 import re
 from pprint import pprint
 
-lines = [items.strip() for items in open('../input/04.txt').read().splitlines()]
+lines = [items.strip() for items in open('../input/04e.txt').read().splitlines()]
 lines = [re.split(':|,|;|\|', line) for line in lines]
 tickets = []
 
@@ -33,18 +33,32 @@ def part2(tickets):
                 ts += 1
                 # print(t, "win:", nr)
         t_score.append(ts)
-    return(t_score)
 
-tic = []
-for n in part2(tickets):
-    tic.append([1,n])
+    # tic: [ticket_count, win_count] = [[1,4], [1,2], [1,2], [1,1], [1,0], [1,0]]
+    tic = []
+    for n in t_score:
+        tic.append([1,n])
 
-# tic = [[1,4], [1,2], [1,2], [1,1], [1,0], [1,0]]
-for n, t in enumerate(tic):
-    for x in range(n+1, t[1]+n+1):
-        tic[x][0] += tic[n][0]
+    for n, t in enumerate(tic):
+        for x in range(n+1, t[1]+n+1):
+            tic[x][0] += tic[n][0]
 
-som = 0
-for n in tic:
-    som += n[0]
-print(som)
+    som = 0
+    for n in tic:
+        som += n[0]
+    return(som)
+# print("Part 2:", part2(tickets))
+
+def part2_improved(input):
+    m = {}
+    for i,x in enumerate(input):
+        if i not in m:
+            m[i] = 1
+
+        j = sum(q in x[0] for q in x[1])
+
+        for n in range(i+1,i+j+1):
+            m[n] = m.get(n,1)+m[i]
+
+    return sum(m.values())
+print("Part 2 improved:", part2_improved(tickets))
