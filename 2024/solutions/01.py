62d3b3838d49248ba5523a14be189d021609bde8
Herman van der Veer <herman.van.der.veer@rabobank.com>
2024-12-09 14:14:27 +0100

    solution 01 - 2024

---
+++ 2024/solutions/01.py
@@ -0,0 +1,24 @@
+# --- Day 1: Historian Hysteria ---
+# To find the total distance between the left list and the right list, add up the distances between all of the pairs you found.
+import re
+
+lines = [items.strip() for items in open('../input/01.txt').read().splitlines()]
+lines = [list(map(int, re.split('   ', line))) for line in lines]
+transposed = list(map(list, zip(*lines)))
+
+
+def a(transposed):
+    transposed[0].sort()
+    transposed[1].sort()
+    distance = 0
+    for a in range(len(transposed[0])):
+        distance += abs(transposed[1][a] - transposed[0][a])
+    print(f"Total distance: {distance}")
+a(transposed)
+
+# --- Part Two ---
+
+product_sum = 0
+for x in transposed[0]:
+    product_sum += x * transposed[1].count(x)
+print(f"Similarity score: {product_sum}")
